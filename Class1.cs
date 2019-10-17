2620d545f7ad312cffb65bfd737b86f380927f11
rkoyyalamudi1747 <55258102+rkoyyalamudi1747@users.noreply.github.com>
2019-10-17 15:33:22 -0400

    Add files via upload
    find

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMax1
+{
+    public class FindMaxNumber
+    {
+        public int FindLargest(int num1, int num2)
+        {
+
+            int result;
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+            return result;
+        }
+    }
+}
